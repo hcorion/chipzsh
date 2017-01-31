a475c3d965cdc51f9f205a9983c13a87c671c0fb
Zion Nimchuk <zionnimchuk@gmail.com>
2017-01-31 13:41:14 -0800

    Brix rom now runs all the way through

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # chipsh
 A chip-8 emulator written in Bash
+
+This script was developed in Zsh, but would most likely run in Bash or sh.
+
+Some command line tools that are required are:
+- od (for reading the binary of the file)
--- main.zsh
+++ main.zsh
@@ -42,7 +42,10 @@ PC=512
 # There is also an index register! Hooray!
 I=0
 
-# These are unused right now, maybe I'll use them in the future.
+# We also need a stack, hooray!
+declare -a stack
+# Aaand a stack pointer 
+sp=1
 
 # There are 15 general purpose registers.
 # There is also a 16th register for carry operations.
@@ -99,8 +102,23 @@ do
     echo "We're currently at register: $address"
     case ${address:0:1} in
         (0)
-            echo "Moving PC to: ${address:1:3}"
-            echo "DUDE!"
+            case ${ram[`expr $PC + 1`]:0:2} in
+                (ee)
+                    echo "Returning from subroutine."
+                    PC=$stack[`expr $sp - 1`]
+                    ;;
+                (e0)
+                    echo "LOL SUPPOSED TO CLEAR THE SCREEN HERE :P"
+                    echo "Drawing pixels is not yet implemented."
+                    ((PC+=2))
+                    ;;
+                *)
+                    echo "YOUR NOT SUPPOSED TO TRIGGER"
+                    echo "${ram[`expr $PC + 1`]:0:2}"
+                    done=1
+                    ;;
+            esac
+
         ;;
         (1)
             # Format: 1NNN
@@ -122,8 +140,24 @@ do
             ((PC+=2))
             ;;
         (2)
-            echo "Calling subroutines not implemented yet. Skipping."
-            ((PC+=2))
+            # Format: 2NNN
+            # Calls subroutine at NNN.
+            stack[sp]=$PC
+            ((sp++))
+            PC=$((16#${address:1:2}${ram[`expr $PC + 1`]:0:2}))
+            echo "Calling subroutine at address $PC"
+            ;;
+        (3)
+            # Format: 3XNN
+            # Skips the next instruction if data register X is equal to NN. 
+            register=$reg[`expr $((16#${address:1:2})) + 1`]
+
+            if [ $register -eq $((16#${ram[`expr $PC + 1`]:0:2})) ]
+            then
+                ((PC+=4))
+            else
+                ((PC+=2))             
+            fi
             ;;
         (6)
             # Format: 6XNN
@@ -133,16 +167,66 @@ do
             reg[`expr $((16#${address:1:2})) + 1`]=$((16#${nextAddress}))
             ((PC+=2))
             ;;
-        (rm -rf*) echo "I just dodged a bullet";;
+        (7)
+            # Format: 7XNN
+            # Adds NN to data register X.
+            toAdd=$((16#${ram[`expr $PC + 1`]:0:2}))
+            regAddress=`expr $((16#${address:1:2})) + 1`
+            echo "Adding $toAdd to data register #$regAddress which is $reg[$regAddress]"
+            added=`expr $toAdd + $reg[$regAddress]`
+            # We need to implement integer overflow.
+            if [ `expr $added` -ge 255 ]
+            then
+                reg[$regAddress]=`expr $added - 255`
+            else
+                reg[$regAddress]=$added
+            fi
+            echo "Register is now: $reg[$regAddress]"
+            ((PC+=2))
+            ;;
+        (f)
+            # The opcode to rule them all!
+            case ${ram[`expr $PC + 1`]:0:2} in
+                (29)
+                    # Format: FX29
+                    # Sets I to the location of the sprite for the character in VX. Characters 0-F (in hexadecimal) are represented by a 4x5 font.
+                    data=`expr $((16#${address:1:2})) + 1`
+                    I=`expr $reg[$data] \* 5`
+                    ((PC+=2))
+                    ;;
+                (33)
+                    # Look up this one, I just copied a pre-made solution.
+                    number=$reg[`expr $((16#${address:1:2})) + 1`]
+                    echo "number = $number"
+                    echo "I = $I"
+                    for i in {3..1}
+                    do
+                        echo "$i"
+                        ram[((`expr $I + $i` + 1))]=`expr $number % 10`
+                        number=`expr $number / 10`
+                    done
+                    ((PC+=2))
+                    ;;
+                (65)
+                    # Format: FX65
+                    # Fills data register 0 to X (including X) with values from memory starting at address I.
+                    echo "reg before" $reg
+                    for i in {1..`expr $((16#${address:1:2})) + 1`}
+                    do
+                        reg[$i]=$ram[`expr $I + $i - 1`]
+                    done
+                    echo "reg after" $reg
+                    ((PC+=2))
+                    ;;
+                *)
+                    echo "F is unimplemented. Calling: ${address:1:2}${ram[`expr $PC + 1`]:0:2}"
+                    done=1
+                    ;;
+            esac
+            ;;
         *)
             echo "Looks like an unknown operation: ${address:0:1}"
             done=1
-  ;;
+            ;;
     esac
-
-
-    if [ $PC -ge 614 ]
-    then
-        done=1
-    fi
 done
